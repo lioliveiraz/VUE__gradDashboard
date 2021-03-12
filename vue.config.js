e1d9e75bd1a5208e0398e3b74bb9877f2b2d6866
lioliveiraz <ligia.o.souza@gmail.com>
2021-03-12 16:06:49 +0100

    comiting

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    pluginOptions: {
+        i18n: {
+            locale: 'pt',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: false,
+            enableLegacy: false
+        }
+    }
+}
